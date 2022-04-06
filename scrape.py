1e3d871c138dfe7ce2d847b2ee703acc5890c9a8
Benno4president <krijoh.b@gmail.com>
2022-04-06 20:22:02 +0200

    fixed filenaming

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import json, re, time, csv
+import json, re, time, csv, os
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,8 +14,8 @@ config = {
             'Families'
         ],
         'start_at': 1,
-        'stop_at': 3,
-        'use_filter': True, # all stories will be saved if false, because the word list is then ignored.
+        'stop_at': 300,
+        'use_filter': False, # all stories will be saved if false, because the word list is then ignored.
         'word_filter': set([ # don't use spaces, they have no effect.
             'mushroom', 
             'mushrooms', 
@@ -41,69 +41,78 @@ def removeHTML(s):
         s = re.sub(htmlRegex, '', s)
     return s
 
+def save_csv(fp, data_ldict):
+    csv_columns = data_ldict[0].keys()
+    with open(fp, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in data_ldict:
+            writer.writerow(data)
 
+def scrape_exp_table(tablenum:int=66):
+    exp_table_soup = REPORT_QUERY_LIST_URL % (66, 1, config['start_at'], config['stop_at'])
+    exp_table_soup = get_soup(exp_table_soup)
+    head_title = exp_table_soup.find('title').string
+    entries = exp_table_soup.find_all('a', href=True)
+    eid_list = []
+    for entry in entries:
+        eid = entry.get('href').split('ID=')
+        if len(eid) > 1:
+            eid_list.append(eid[1])
+    print('Found ids:', len(eid_list))
 
-exp_table_soup = REPORT_QUERY_LIST_URL % (66, 1, config['start_at'], config['stop_at'])
-exp_table_soup = get_soup(exp_table_soup)
+    story_list = []
+    for expid in eid_list:
+        print('Fetching id: %s' % expid)
+        page = str(requests.get(EXP_BASE_URL % expid).content)
 
-entries = exp_table_soup.find_all('a', href=True)
-eid_list = []
-for entry in entries:
-    eid = entry.get('href').split('ID=')
-    if len(eid) > 1:
-        eid_list.append(eid[1])
-print('Found ids:', len(eid_list))
+        # sleeping to not get ip banned for spam
+        time.sleep(0.5)
+        if 'Unable to view experience' in page:
+            print('No report at that ID number.')
+            continue
+        intrsec = set(page.lower().split()).intersection(config['word_filter'])  
+        if not config['use_filter']:
+            pass
+        elif not intrsec: 
+            # this checks if a word from the list is in the page anywhere.
+            print('No search keyword found.')
+            continue 
+        else:
+            print('Matched on:', intrsec) 
 
+        
+        title = page.split('class="title">')[1].split('</div>')[0].replace('\n', '')
+        substance = page.split('class="substance">')[1].split('</div>')[0].replace('\n', '')
+        author = page.split('class="author">')[1].split('</a>')[0].split('>')[1].replace('\n', '')
+        
+        print('Title: %s'%title)
+        print('Substance: %s'%substance)
+        print('Author: %s'%author)
 
-story_list = []
-for expid in eid_list:
-    print('Fetching id: %s' % expid)
-    page = str(requests.get(EXP_BASE_URL % expid).content)
+        # get main text and remove common unicode characters
+        body = page.split('Start Body -->')[1].split('<!-- End Body')[0]
+        body = body.replace('\r','')
+        body = body.replace('\n','')
+        body = body.replace('\x92',"'")
+        body = body.replace('\x93','"')
+        body = body.replace('\x94','"')
+        body = body.replace('\x97',' -- ')
+        body = removeHTML(body)
+        body = body.strip()
 
-    # sleeping to not get ip banned for spam
-    time.sleep(0.5)
-    if 'Unable to view experience' in page:
-        print('No report at that ID number.')
-        continue
-    intrsec = set(page.lower().split()).intersection(config['word_filter'])  
-    if not config['use_filter']:
-        pass
-    elif not intrsec: 
-        # this checks if a word from the list is in the page anywhere.
-        print('No search keyword found.')
-        continue 
-    else:
-        print('Matched on:', intrsec) 
+        story_list.append({'id':expid, 'title':title, 'author':author, 'substance':substance, 'story': body})
+    return head_title, story_list
 
+if __name__ == '__main__':
+    filename = './table_csvs/%s.csv'
+    table_list = [66]
+    if not os.path.exists('./table_csvs/'):
+        os.mkdir('./table_csvs/')
+        print('created dir ./table_csvs')
+    for table_id in table_list:
+        title, data = scrape_exp_table(table_id)
+        title = title.replace(' ', '_').replace('.', '').split(':')[0]
+        save_csv(filename%title, data)
+        print('table saved to', filename%title)
     
-    title = page.split('class="title">')[1].split('</div>')[0].replace('\n', '')
-    substance = page.split('class="substance">')[1].split('</div>')[0].replace('\n', '')
-    author = page.split('class="author">')[1].split('</a>')[0].split('>')[1].replace('\n', '')
-    
-    print('Title: %s'%title)
-    print('Substance: %s'%substance)
-    print('Author: %s'%author)
-
-    # get main text and remove common unicode characters
-    body = page.split('Start Body -->')[1].split('<!-- End Body')[0]
-    body = body.replace('\r','')
-    body = body.replace('\n','')
-    body = body.replace('\x92',"'")
-    body = body.replace('\x93','"')
-    body = body.replace('\x94','"')
-    body = body.replace('\x97',' -- ')
-    body = removeHTML(body)
-    body = body.strip()
-
-    story_list.append({'id':expid, 'title':title, 'author':author, 'substance':substance, 'story': body})
-
-
-# This converts and saves the data as csv
-with open(f'scraped_{len(story_list)}.csv', 'w') as csv_file:  
-    writer = csv.writer(csv_file)
-    writer.writerow(list(story_list[0].keys()))
-    for story_page in story_list:
-        writer.writerow([story_page['id'], story_page['title'], story_page['author'], story_page['substance'], story_page['story']])
-
-
-
